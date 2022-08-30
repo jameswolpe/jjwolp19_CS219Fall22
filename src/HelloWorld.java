166581f18f55ed9bb1e62c114f4b9e259bff3101
jameswolpe <jjwolp19@stlawu.edu>
2022-08-30 15:14:20 -0400

    more updates

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,18 @@
 // by convention, class names are capitalized.
 public class HelloWorld {
+    /*
+        This is a multiline block comment.
+        Another comment line.
+     */
+
     public static void main(String [] args) {
         System.out.println("Hello World");
         int x = 5;
         boolean z = true;
         double pi = 3.14159;
+        String name = "CS219 Techniques of C0mputer Science";
+        double avagadros = 6.022140857e23; //Scientific notation
+        double electron_mass = 9.1093837015e-31;
+
     }
 }
